3630e47fb5adc74407f663bb17e605c13c167b12
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-10 12:13:54 +0500

    Hooks created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 
 import './App.css'
+import RandomPasswordGen from './components/random_password_gen'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <h1>Hello World</h1>
+      <RandomPasswordGen/>
+      
     </>
   )
 }
--- src/components/random_password_gen.jsx
+++ src/components/random_password_gen.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { useState } from 'react'
 
 const RandomPasswordGen = () => {
 
+  const [length,setLength] =  useState(15);
+  const [numberAllowed , setNumberAllowed] = useState(false);
+  const [charAllowed , setCharAllowed] = useState(false);
+  const [password,setPassword] = useState("");
 
-  
+
   return (
     <>
-    
+    <h1 className='text-4xl text-center'>Password Generator</h1>
     </>
   )
 }
